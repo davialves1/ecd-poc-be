6b5b87cf8e7a309f5654a1769b01e79141b7a5e3
Davi Alves <extern.davi.alves@volkswagen.de>
2022-07-26 12:22:51 +0200

    created basic file read logic

--- src/main/java/com/example/pocbackend/controller/FileUploadController.java
+++ src/main/java/com/example/pocbackend/controller/FileUploadController.java
@@ -1,5 +1,8 @@
 package com.example.pocbackend.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,9 +17,25 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import org.apache.commons.codec.digest.XXHash32;
+import org.apache.poi.hpsf.NoPropertySetStreamException;
+import org.apache.poi.hpsf.PropertySet;
+import org.apache.poi.hpsf.SummaryInformation;
+import org.apache.poi.hpsf.UnexpectedPropertySetTypeException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.openxml4j.opc.PackageProperties;
+import org.apache.poi.poifs.filesystem.DirectoryEntry;
+import org.apache.poi.poifs.filesystem.DocumentEntry;
+import org.apache.poi.poifs.filesystem.DocumentInputStream;
+import org.apache.poi.poifs.filesystem.FileMagic;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -37,25 +56,74 @@ public class FileUploadController {
 
 	@CrossOrigin()
 	@PostMapping("/upload")
-	public ResponseEntity<MetaDataDTO> uploadFile(MultipartFile file) throws IOException, InvalidFormatException {
+	public ResponseEntity<MetaDataDTO> uploadFile(MultipartFile file)
+		throws IOException, InvalidFormatException, NoPropertySetStreamException, UnexpectedPropertySetTypeException {
 		Path path = Paths.get("/Users/davialves/Desktop/Volkswagen/ECD/poc-backend/file.xlsx");
 		file.transferTo(path);
-		BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
-		OPCPackage pckg = OPCPackage.open(file.getInputStream());
-		PackageProperties props = pckg.getPackageProperties();
-		System.out.println(props.toString());
-		MetaDataDTO metaDataDTO = new MetaDataDTO(
-			file.getOriginalFilename(),
-			formatDate(props.getCreatedProperty().orElse(new Date())),
-			formatDate(props.getModifiedProperty().orElse(new Date())),
-			props.getCreatorProperty().orElse("")
-		);
+		FileInputStream inputStream = new FileInputStream(path.toFile());
 
+		MetaDataDTO metaDataDTO;
+
+		// We are checking the file format OLE2(newer) or OOXML(older)
+		if (FileMagic.valueOf(inputStream).equals(FileMagic.OOXML)) {
+			POIFSFileSystem poifs = new POIFSFileSystem(inputStream);
+			DirectoryEntry dir = poifs.getRoot();
+			DocumentEntry siEntry =      (DocumentEntry)dir.getEntry(SummaryInformation.DEFAULT_STREAM_NAME);
+			DocumentInputStream dis = new DocumentInputStream(siEntry);
+			PropertySet ps = new PropertySet(dis);
+			SummaryInformation si = new SummaryInformation(ps);
+			String author = si.getAuthor();
+			inputStream.close();
+			System.out.println("Author: " + author);
+			System.out.println("Summary Information: " + si);
+			metaDataDTO = new MetaDataDTO(
+				file.getOriginalFilename(),
+				formatDate(si.getCreateDateTime()),
+				formatDate(si.getLastSaveDateTime()),
+				si.getAuthor()
+			);
+		} else {
+			OPCPackage opcPackage = OPCPackage.open(inputStream);
+			PackageProperties props = opcPackage.getPackageProperties();
+			System.out.println(props.toString());
+			metaDataDTO = new MetaDataDTO(
+				file.getOriginalFilename(),
+				formatDate(props.getCreatedProperty().orElse(new Date())),
+				formatDate(props.getModifiedProperty().orElse(new Date())),
+				props.getCreatorProperty().orElse("")
+			);
+			opcPackage.close();
+		}
+
+		readExcelFile();
 		return new ResponseEntity<MetaDataDTO>(metaDataDTO, HttpStatus.OK);
+
 	}
 
 	public String formatDate(Date date) {
 		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
 		return dateFormat.format(date);
+	}
+
+	public void readExcelFile() throws IOException {
+		FileInputStream inputStream = new FileInputStream(new File("/Users/davialves/Desktop/Volkswagen/ECD/poc-backend/file.xlsx"));
+		Workbook workbook = WorkbookFactory.create(inputStream);
+		Sheet sheet = workbook.getSheetAt(0);
+		String header = sheet.getHeader().toString();
+		Integer firstRowNumber = sheet.getFirstRowNum();
+		Integer lastRowNumber = sheet.getLastRowNum();
+		Row row = sheet.getRow(0);
+
+//		Log values
+		System.out.println("-------------------");
+		System.out.println("sheet - " + sheet);
+		System.out.println("-------------------");
+		System.out.println("header - " + header.toString());
+		System.out.println("-------------------");
+		System.out.println("firstRowNumber - " + firstRowNumber);
+		System.out.println("-------------------");
+		System.out.println("lastRowNumber - " + lastRowNumber);
+		System.out.println("-------------------");
+		System.out.println("row - " + row.getCell(0));
 	}
 }
